966a1c765e40a583c1805fd50fd99f95bb085061
André Kilchenmann <github@milchkannen.ch>
2019-03-24 12:29:16 +0100

    feat: add playground for action: message

---
+++ src/app/action-pg/action-pg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kuip-action-pg',
+  templateUrl: './action-pg.component.html',
+  styleUrls: ['./action-pg.component.scss']
+})
+export class ActionPgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
