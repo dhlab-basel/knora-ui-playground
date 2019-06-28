64efd695776858c918742d34037414caad3b85a5
André Kilchenmann <github@milchkannen.ch>
2019-06-28 08:37:29 +0200

    chore(resource viewer): Adding latest version of Resource object

--- src/app/viewer-pg/viewer-pg.component.ts
+++ src/app/viewer-pg/viewer-pg.component.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Params, ActivatedRoute, Router } from '@angular/router';
-import { ReadResourcesSequence, OntologyInformation, KnoraConstants, ResourceService, IncomingService, ApiServiceError, ReadResource, StillImageRepresentation, ReadStillImageFileValue, ImageRegion, GuiOrder, OntologyCacheService } from '@knora/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'kuip-viewer-pg',
-  templateUrl: './viewer-pg.component.html',
-  styleUrls: ['./viewer-pg.component.scss']
+    selector: 'kuip-viewer-pg',
+    templateUrl: './viewer-pg.component.html',
+    styleUrls: ['./viewer-pg.component.scss']
 })
 export class ViewerPgComponent implements OnInit {
 
-  testResourceIri: string = 'http%3A%2F%2Frdfh.ch%2F0803%2F6ad3e2c47501';
+    pageResourceIri: string = 'http%3A%2F%2Frdfh.ch%2F0803%2F6ad3e2c47501';
+    bookResourceIri: string = 'http%3A%2F%2Frdfh.ch%2F0803%2F2a6221216701';
+    testResourceIri: string;
 
-  resourceIri: string;
+    resourceIri: string;
 
-  constructor(
-    private _route: ActivatedRoute
-  ) { }
+    constructor (
+        private _route: ActivatedRoute
+    ) { }
 
-  ngOnInit() {
-    this._route.paramMap.subscribe(
-      (params: Params) => {
-        this.resourceIri = (decodeURIComponent(params.get('id')) === null) ? this.testResourceIri : decodeURIComponent(params.get('id'));
+    ngOnInit() {
+        this.resourceIri = this.pageResourceIri;
+        this._route.paramMap.subscribe(
+            (params: Params) => {
+                this.resourceIri = (decodeURIComponent(params.get('id')) === null) ? this.testResourceIri : decodeURIComponent(params.get('id'));
 
-        // console.log(this.resourceIri)
-      }
-    );
-  }
+                // console.log(this.resourceIri)
+            }
+        );
+    }
 
 }
